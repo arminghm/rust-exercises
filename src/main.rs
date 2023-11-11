96a450a1548a69dcdd82d364ba2e29acb5b4ca4a
arminghm <armin.ghoreishi@gmail.com>
2023-11-11 14:44:21 +0330

    add reverse string exercise

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 use clap::Parser;
 
 mod command;
-
-use command::{Command, AppCommands};
+use command::{AppCommands, Command};
 
 fn main() {
-    let arg = Command::parse();
-    match arg.command {
+  println!("{}", reverse("input"))
+}
+
+#[allow(dead_code)]
+pub fn reverse(input: &str) -> String {
+  /*This is a better solution */
+
+  input.chars().rev().collect::<String>()
+
+  /*This is the previous solution */
+  // let mut output = String::new();
+  // for char in input.chars() {
+  //   output = char.to_string() + &output;
+  // }
+  // return output;
+}
+
+#[allow(dead_code)]
+fn command_line() {
+  let arg = Command::parse();
+  match arg.command {
+    Some(i) => match i {
+      AppCommands::User(user_command) => match user_command.command {
         Some(i) => match i {
-            AppCommands::User(user_command) => {
-                match user_command.command {
-                    Some(i) => match i {
-                        command::user::UserSubCommand::Create(user) => {
-                            println!("create a user with name: {}, age: {}", user.name, user.age)
-                        }
-                        command::user::UserSubCommand::Update => {
-                            println!("Update command ")
-                        }
-                    },
-                    None => {}
-                }
-            }
+          command::user::UserSubCommand::Create(user) => {
+            println!("create a user with name: {}, age: {}", user.name, user.age)
+          }
+          command::user::UserSubCommand::Update => {
+            println!("Update command ")
+          }
         },
-        None => {
-            println!("No command specified!")
-        }
+        None => {}
+      },
+    },
+    None => {
+      println!("No command specified!")
     }
+  }
 }
